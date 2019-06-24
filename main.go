a2f0ad342f0ce83d959019d461a444cc08a3d368
George Carter <minecsteve@yahoo.com>
2019-06-24 18:05:00 -0400

    Added Auth file and discord bot init

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"io"
+	"os"
+)
+
+func main(){
+	
+	// open auth file
+	file, err := os.Open("auth.txt")
+	// error check
+    if err != nil {
+        log.Fatal(err)
+    }
+	defer file.Close()
+	
+	// read file
+	token, err := io.Read(file)
+	if err != nil {
+        log.Fatal(err)
+    }
+
+	discord, err := discordgo.New("Bot " + token)
+}
