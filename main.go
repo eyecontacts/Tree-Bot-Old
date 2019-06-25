8f5ec938e733f78a56c5328ff4749bfc6810f122
eyecontacts <minecsteve@yahoo.com>
2019-06-24 21:17:55 -0400

    Fixed file token import
    Also added more error checking

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
 	"log"
 	"os"
-
-	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
 
 	// open auth file
-	file, err := os.Open("Auth.txt")
+	file, err := os.Open("auth.txt")
 	// error check
 	if err != nil {
 		log.Fatal(err)
@@ -18,14 +18,20 @@ func main() {
 	defer file.Close()
 
 	// read file
-	token := make([]byte, 100)
+	token := make([]byte, 59)
 	_, err = file.Read(token)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	discord, err := discordgo.New("Bot " + string(token))
+	discord, err := discordgo.New(string(token))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// init bot
 	err = discord.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
